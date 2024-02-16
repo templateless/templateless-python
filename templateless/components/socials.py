800c02c7ee29a6b8a3cb0afb163e6b516a55e784
Ivan Akimov <ivan@templateless.com>
2024-02-16 10:53:26 -0600

    chore: new socials services

--- templateless/components/socials.py
+++ templateless/components/socials.py
@@ -5,9 +5,20 @@ from ..common import ComponentId, Component
 class Service(Enum):
     WEBSITE = "WEBSITE"
     EMAIL = "EMAIL"
+    PHONE = "PHONE"
+    FACEBOOK = "FACEBOOK"
+    YOUTUBE = "YOUTUBE"
     TWITTER = "TWITTER"
     X = "X"
     GITHUB = "GITHUB"
+    INSTAGRAM = "INSTAGRAM"
+    LINKEDIN = "LINKEDIN"
+    SLACK = "SLACK"
+    DISCORD = "DISCORD"
+    TIKTOK = "TIKTOK"
+    SNAPCHAT = "SNAPCHAT"
+    THREADS = "THREADS"
+    TELEGRAM = "TELEGRAM"
 
 
 class Item:
